9b43f839570cd743550fbb5aabb32725dfe4ac6f
Tugsbileg <tugu0405@gmail.com>
2016-10-26 14:15:39 +0800

    ''

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
-
+import request from 'superagent';
 export default class Content extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: []
+    };
+  }
+
+  componentDidMount() {
+    var component = this;
+    var url = "http://blogapi-92244.onmodulus.net/api/articles?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpYXQiOjE0NzcyMjU4Mzl9.LC1tbqL6vHvPqA2GQ-cjmWAJC1TGvM3CGe88WhzfHHw";
+    request
+      .get(url)
+      .end(function (err, res) {
+        console.log(res);
+        var response = res.body;
+        component.setState({
+          articles: response
+        })
+      });
+  }
+
   render() {
-    return(
+    var news = this.state.articles.map(function (article) {
+      return (
+        <div key={article._id}>
+          <h2>{article.caption}</h2>
+          <p>{article.description}</p>
+        </div>
+      );
+    });
+    return (
       <div>
-        <h2>Content</h2>
+        <h2>{news}</h2>
       </div>
     )
   }
