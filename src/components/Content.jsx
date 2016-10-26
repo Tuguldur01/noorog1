4281ae823e7e37b85ced0e5fd534d2ef975fa03f
Tugi <tuguldur.01@gmail.com>
2016-10-25 22:57:27 -0700

    content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router'
 
 export default class Content extends React.Component {
   render() {
     return(
       <div>
-        <h2>Content</h2>
+        <h2>garchig</h2>
+        <Link to="/post/1">Дэлгэрэнгүй</Link>
       </div>
     )
   }
---
+++ src/components/Css.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default class Css extends React.Component {
+
+
+  render() {
+    return(
+      <div>
+        <img src="http://lorempixel.com/1000/800" alt="" />
+          <div className="header header-filter">
+        		<div className="container">
+        			<div className="row">
+        				<div className="col-md-8 col-md-offset-2">
+        					<div className="brand">
+        						<h1>Material Kit.</h1>
+        						<h3>A Badass Bootstrap UI Kit based on Material Design.</h3>
+        					</div>
+        				</div>
+        			</div>
+
+        		</div>
+        	</div>
+        <div className="main main-raised">
+          <div className="section section-basic">
+              <div className="container">
+          <h1>qwe</h1>
+            <h1>qwe</h1>
+              <h1>qwe</h1>
+                <h1>qwe</h1>
+                  <h1>qwe</h1>
+                    <h1>qwe</h1>
+                    </div>
+                  </div>
+        </div>
+      </div>
+    )
+  }
+}
